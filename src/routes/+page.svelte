bbba329e6d446b8fa67529bd8a34bf8fc5174ca8
Cheru Berhanu <cheru@cheru.dev>
2022-12-01 13:39:40 -0500

    aaa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
   import Snow from "../components/Snow.svelte";
   import Flag from "../components/Flag.svelte";
+  import "normalize.css/normalize.css"
 
   import { library } from '@fortawesome/fontawesome-svg-core';
-  import { faMapLocationDot, faClock, faSeedling } from '@fortawesome/free-solid-svg-icons';
+  import { faMapLocationDot, faClock, faSeedling, faClipboardCheck } from '@fortawesome/free-solid-svg-icons';
   import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from 'fontawesome-svelte';
 
   let main2;
 
-  library.add(faMapLocationDot, faClock, faSeedling);
+  library.add(faMapLocationDot, faClock, faSeedling, faClipboardCheck);
 </script>
 
 <svelte:head>
@@ -18,9 +19,10 @@
 <Flag />
 <div class="main" style="overflow: hidden">
     <div class="content" style="color: white">
-        <h1 style="margin: 0; font-family: 'JetBrains Mono', monospace">Join other high <br>schoolers on <br><strong>December 30</strong> <br>
+        <h1 style="margin: 0; font-family: 'JetBrains Mono', monospace; ">Join other high <br>schoolers on <br><strong>December 30</strong> <br>
             for 10 hours of <br>fun & making at <br>
             <strong>Epoch VT!</strong> </h1>
+        <h3 style="margin: 3% 0 0 0">(register below)</h3>
         <svg fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" xmlns="http://www.w3.org/2000/svg" aria-label="down-caret" viewBox="0 0 32 32" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="50" height="50" color="white" style="cursor: pointer" on:click={() => main2.scrollIntoView({behavior: "smooth"})}><g><path d="M 0.359841 9.01822C 0.784113 9.37178 1.41467 9.31446 1.76823 8.8902C 3.14518 7.2451 6.52975 3.42464 8.25002 2.11557C 9.99919 3.44663 13.335 7.21555 14.7318 8.8902C 15.0854 9.31446 15.7159 9.37178 16.1402 9.01822C 16.5645 8.66466 16.6215 8.03371 16.2679 7.60943C 14.7363 5.76983 11.2749 1.80977 9.30351 0.408618C 8.99227 0.190441 8.64018 0 8.25002 0C 7.85987 0 7.50778 0.190441 7.19654 0.408618C 5.26486 1.78153 1.73514 5.80788 0.232849 7.60856L 0.231804 7.60982C -0.12176 8.03409 -0.0644362 8.66466 0.359841 9.01822Z" transform="translate(7.12506 20.6251) scale(1 -1)"></path></g></svg>
         </div>
     <div class="quick-info">
@@ -34,13 +36,13 @@
     <div class="quick-info mobile-only">
         <h2 style="text-align: center"><strong><FontAwesomeIcon icon={faMapLocationDot} /></strong> 15 Falls Road, Shelburne VT<br>
             <strong><FontAwesomeIcon icon={faClock} /></strong> December 30, 10am-8pm<br>
-            <strong><FontAwesomeIcon icon={faSeedling} /></strong> Beginners Welcome<br>
+            <strong><FontAwesomeIcon icon={faSeedling} /></strong> Beginners Welcome
         </h2>
     </div>
     <div>
     <h1>The Rundown:</h1>
         <p><strong>Epoch VT</strong> is a 10-hour hackathon (social coding event) where teens will come together to discover the joy of code, build creative projects, learn new skills, and build connections with one another. <br><br>It's taking place at <strong><a href="https://goo.gl/maps/6GnAJgtejh7qqH726">Hack Club HQ</a></strong> (15 Falls Road, Shelburne) from <strong>10AM to 8PM</strong> on <strong>December 30, 2022</strong>. Whether you've never coded before or you're a seasoned developer, we'd love to have you join us!
-        <br><br>It's one of many regional events surrounding <a href="https://epoch.hackclub.com">Epoch</a>, a large high-school hackathon taking place in Dehli, India, which is organized by <a href="https://hackclub.com">Hack Club</a>.
+        <br><br>It's one of many regional events surrounding <a href="https://epoch.hackclub.com">Epoch</a>, a large high-school hackathon taking place in Dehli, India. It's organized by <a href="https://hackclub.com">Hack Club</a>, a nonprofit network of high school coding clubs and makers around the world.
         <br><br>Everything at this event is totally free, including lunch, dinner, swag (hoodies, T-shirts, & stickers!), and other goodies. Our finances are all publicly visible on <a href="https://bank.hackclub.com/epochvt">Hack Club Bank</a>.
         <br><br>If you have any questions, feel free to email Cheru Berhanu at <a href="mailto:cheru@cheru.dev">cheru@cheru.dev</a>. <br>This is going to be a ton of fun! We hope to see you there!
         <br><br>- <a href="https://cheru.dev">Cheru Berhanu</a>, <a href="https://henrybass.github.io/">Henry Bass</a>, and <a href="https://snwy.me">Mason Meirs</a> :D
@@ -75,6 +77,7 @@
 
     .content {
         margin: 2em;
+
     }
 
     strong, a {
